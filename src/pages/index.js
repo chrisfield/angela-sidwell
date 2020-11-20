0fbb34413202607d12dc2abbf852690fd1cdf5bf
Kit Field <christopher.field@radiuspaymentsolutions.com>
2020-11-20 20:27:11 +0000

    Add GNCCF link to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 
 export default ({ data }) => (
   <Layout>
+      <div style={{ marginBottom: '2em' }}>
+        <div>
+          View/purchase my work at Great Northern Contemporary Craft Fair Online <Link to="/gnccf2">here</Link>
+        </div>
+      </div>
       <Img
         fluid={data.file.childImageSharp.fluid}
         alt="fox"
